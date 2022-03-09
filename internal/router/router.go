3f013b3959c0fdd6f6d329c5d4210c8e3ca0b5df
Lucas Lehot <lehotlucas@gmail.com>
2022-03-09 10:56:31 +0100

    add(routes): #15 - create services data routes

--- internal/router/router.go
+++ internal/router/router.go
@@ -23,20 +23,22 @@ func usersRoute(r *gin.Engine) {
 	r.DELETE("/users/:id", controllers.DeleteUser)
 
 	r.POST("/users/:user_id/display-preferences", controllers.CreateDisplayPreferences)
-	
 	r.GET("/users/:user_id/display-preferences", controllers.GetDisplayPreferences)
-	
 	r.PUT("/users/:user_id/display-preferences", controllers.UpdateDisplayPreferences)
-	
 	r.DELETE("/users/:user_id/display-preferences", controllers.DeleteDisplayPreferences)
 
 	r.POST("/users/:user_id/active-services", controllers.CreateActiveServices)
-	
 	r.GET("/users/:user_id/active-services", controllers.GetActiveServices)
-	
 	r.PUT("/users/:user_id/active-services", controllers.UpdateActiveServices)
-	
 	r.DELETE("/users/:user_id/active-services", controllers.DeleteActiveServices)
+
+	r.POST("/users/:user_id/sw-services-data", controllers.CreateSwServicesData)
+	r.GET("/users/:user_id/sw-services-data", controllers.GetSwServicesData)
+	r.DELETE("/users/:user_id/sw-services-data", controllers.DeleteSwServicesData)
+
+	r.POST("/users/:user_id/aws-services-data", controllers.CreateAwsServicesData)
+	r.GET("/users/:user_id/aws-services-data", controllers.GetAwsServicesData)
+	r.DELETE("/users/:user_id/aws-services-data", controllers.DeleteAwsServicesData)
 }
 
 func secretsManagerRoute(r *gin.Engine) {
