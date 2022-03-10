0d9708ab3f806a683a221a0198cf952764782b88
ArmandBeni <armand.benichou@hetic.net>
2022-03-10 15:58:06 +0100

    feat(web): #8 remove useless elements, comments useless imports

--- web/src/components/dashboard/account-popover.tsx
+++ web/src/components/dashboard/account-popover.tsx
@@ -40,7 +40,7 @@ export const AccountPopover: FC<AccountPopoverProps> = (props) => {
     try {
       onClose?.();
       await logout();
-      router.push('/');
+      router.push('/dashboard');
     } catch (err) {
       console.error(err);
       toast.error('Unable to logout.');
