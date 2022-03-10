41c4c847de20bd6eeb0327ed21021c85ae097805
Lucas Lehot <lehotlucas@gmail.com>
2022-03-10 16:34:28 +0100

    feat(scheduler): #16 - create simple scheduler

---
+++ internal/queue/scheduler.go
@@ -0,0 +1,28 @@
+package queue
+
+import (
+	"time"
+	
+	log "github.com/sirupsen/logrus"
+
+	"github.com/blyndusk/flamingops/internal/database"
+	"github.com/blyndusk/flamingops/pkg/models"
+)
+
+func main() {
+	ticker := time.NewTicker(24 * time.Hour)
+	for ; true; <-ticker.C {
+		sendMessages()
+	}
+}
+
+func sendMessages() {
+	var users []models.User
+	if err := database.Db.Find(&users).Error; err != nil {
+		log.Error(err)
+		return
+	}
+	for _, user := range users {
+		HandleMessageCreation(&user)
+	}
+}
